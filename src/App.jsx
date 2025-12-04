78cfed7ef897c2ff4fc069dd5dbdebb3a407ee9d
Chimoneg27 <chimonegarvin27@gmail.com>
2025-12-04 21:06:09 +0200

    install dotenv

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-pink-400">Kids Haven Book Tracker App</h1>
+      <h1 className="text-black text-5xl font-bold">Kids Haven Book Tracker App</h1>
     </div>
   )
 }
